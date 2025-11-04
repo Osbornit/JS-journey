64e93504cc1b5b9a2efcb8dd4fb0887a584eca88
Osborn <osborn@Osborns-MacBook-Pro.local>
2025-11-04 13:50:03 +0000

    Initial JS commit

---
+++ intro.js
@@ -0,0 +1,6 @@
+function hello(name,age,language)
+{
+  return 'Hello my name is '+ name +', I am '+ age +' years old and my favorite programming language is '+language;
+
+}
+console.log(hello('Osborn',20,'Javascript'));
